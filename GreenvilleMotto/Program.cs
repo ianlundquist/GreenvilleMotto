5e92e406fb0e17c7fb066367722568be41ddd2d8
Ian Lundquist <oe1550uh@go.minnstate.edu>
2020-01-18 19:48:28 -0600

    Add project files.

---
+++ GreenvilleMotto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreenvilleMotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The stars shine in Greenville.");
+        }
+    }
+}
